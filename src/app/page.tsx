6e77620b4a66ec9090ef0c2eb210b499797796b0
M6K4N9R <zubchevskiy@gmail.com>
2024-11-13 15:33:36 +0100

    Test Header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,26 @@
+"use client";
+
 import Image from "next/image";
-// import Header from "./_components/Header";
+import Header from "./_components/Header";
 
 export default function Home() {
   return (
-    
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1 className="font-bold text-3xl">Gift Whisper</h1>
-        <h2 className="text-md">
-          Create and share personalized gift wishlists.
-        </h2>
+    <>
+      <Header
+        navItems={[
+          { label: "About", href: "/about" },
+          { label: "Settings", href: "/settings" },
+          { label: "Profile", href: "/my-profile" },
+        ]}
+      />
+      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+          <h1 className="font-bold text-3xl">Gift Whisper</h1>
+          <h2 className="text-md">
+            Create and share personalized gift wishlists.
+          </h2>
 
-        {/* <div className="flex gap-4 items-center flex-col sm:flex-row">
+          {/* <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -36,32 +45,32 @@ export default function Home() {
             Read our docs
           </a>
         </div> */}
-        <div className="w-full md:w-2/4 p-3"></div>
-        <button className="px-4 py-2 rounded-3xl border hover:border-orange-600 hover:bg-slate-900 transition-all ease-in-out">
-          <svg
-            className="inline-block mr-3"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          >
-            <circle cx="12" cy="12" r="10" />
-            <path d="M8 12h8" />
-            <path d="M12 8v8" />
-          </svg>
-          Create a list
-        </button>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <code className="bg-slate-800 text-white px-4 py-1">
-          ...in development
-        </code>
-        {/* <a
+          <div className="w-full md:w-2/4 p-3"></div>
+          <button className="px-4 py-2 rounded-3xl border hover:border-orange-600 hover:bg-slate-900 transition-all ease-in-out">
+            <svg
+              className="inline-block mr-3"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <circle cx="12" cy="12" r="10" />
+              <path d="M8 12h8" />
+              <path d="M12 8v8" />
+            </svg>
+            Create a list
+          </button>
+        </main>
+        <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+          <code className="bg-slate-800 text-white px-4 py-1">
+            ...in development
+          </code>
+          {/* <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           target="_blank"
@@ -106,7 +115,8 @@ export default function Home() {
           />
           Go to nextjs.org →
         </a> */}
-      </footer>
-    </div>
+        </footer>
+      </div>
+    </>
   );
 }
