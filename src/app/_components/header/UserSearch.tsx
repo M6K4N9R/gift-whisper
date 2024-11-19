5ea8433e962ce5f404676c263b380f26b393a283
M6K4N9R <zubchevskiy@gmail.com>
2024-11-19 15:00:53 +0100

    Delete search icon from /public. Use an inline svg in UserSearch

--- src/app/_components/header/UserSearch.tsx
+++ src/app/_components/header/UserSearch.tsx
@@ -15,38 +15,37 @@ const UserSearch: React.FC<SearchProps> = ({ onSearch }) => {
 
   return (
     <form
-  onSubmit={handleSubmit}
-  className="relative flex items-center w-full mx-4"
->
-  <input
-    type="text"
-    value={query}
-    onChange={(e) => setQuery(e.target.value)}
-    placeholder="Search by name or email"
-    className="w-full pl-4 pr-10 py-2 border rounded-md bg-dark-accent-100 focus:outline-none focus:ring-2 focus:ring-primary-900 focus:border-transparent"
-  />
-  <button 
-    type="submit" 
-    className="absolute right-3 top-1/2 transform -translate-y-1/2"
-  >
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className="text-dark-accent-500 hover:text-primary-900"
+      onSubmit={handleSubmit}
+      className="relative flex items-center w-full mx-4"
     >
-      <circle cx="11" cy="11" r="8" />
-      <path d="m21 21-4.3-4.3" />
-    </svg>
-  </button>
-</form>
-
+      <input
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        placeholder="Search by name or email"
+        className="w-full pl-4 pr-10 py-2 border rounded-md bg-dark-accent-100 focus:outline-none focus:ring-2 focus:ring-primary-900 focus:border-transparent"
+      />
+      <button
+        type="submit"
+        className="absolute right-3 top-1/2 transform -translate-y-1/2"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="text-dark-accent-500 hover:text-secondary-900"
+        >
+          <circle cx="11" cy="11" r="8" />
+          <path d="m21 21-4.3-4.3" />
+        </svg>
+      </button>
+    </form>
   );
 };
 
