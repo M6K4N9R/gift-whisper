a3009d61f2be832bda6c5ec44620cd8a74ec6883
M6K4N9R <zubchevskiy@gmail.com>
2024-11-21 19:46:17 +0100

    Try alternative layout for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,58 @@ export default function Home() {
   return (
     <>
       <Header />
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <div className="flex flex-col min-h-screen my-auto p-4">
+        <div className="flex-grow flex flex-col justify-center items-center">
+          <h1 className="font-gelica text-3xl md:text-5xl text-white text-center mx-auto">
+            Create your Gift list <br />
+            for any occasion
+          </h1>
+          <h2 className="text-sm md:text-base text-white text-center mx-auto mt-5">
+            Add gifts from any website and share it with anyone
+          </h2>
+        </div>
+        <div className="flex justify-center gap-3 mt-4">
+          <button className="px-4 py-2 rounded-3xl border hover:border-orange-600 hover:bg-slate-900 transition-all ease-in-out">
+            <svg
+              className="inline-block mr-3"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <circle cx="12" cy="12" r="10" />
+              <path d="M8 12h8" />
+              <path d="M12 8v8" />
+            </svg>
+            Create list
+          </button>
+          <button className="px-4 py-2 rounded-3xl border hover:border-orange-600 hover:bg-slate-900 transition-all ease-in-out">
+            <svg
+              className="inline-block mr-3"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <circle cx="12" cy="12" r="10" />
+              <path d="M8 12h8" />
+              <path d="M12 8v8" />
+            </svg>
+            Learn more
+          </button>
+        </div>
+      </div>
+      {/* <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
           <h1 className="font-gelica text-5xl text-white text-center mx-auto">
             Create your Gift list <br />
@@ -41,7 +92,7 @@ export default function Home() {
             ...in development
           </code>
         </footer>
-      </div>
+      </div> */}
     </>
   );
 }
