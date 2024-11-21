d0ea2a412dded81aff11c2d3e78f4dff9288d6b5
M6K4N9R <zubchevskiy@gmail.com>
2024-11-21 20:06:40 +0100

    Implement Buttons on home page. Test. Needs Fixing bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -85,7 +85,7 @@ export default function Home() {
         )}
       </div>
 
-      <div className="flex justify-center gap-3 mt-4">
+      <div className="mt-8 flex flex-col sm:flex-row gap-4">
         <button className="px-4 py-2 rounded-3xl border hover:border-orange-600 hover:bg-slate-900 transition-all ease-in-out">
           <svg
             className="inline-block mr-3"
@@ -95,36 +95,24 @@ export default function Home() {
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           >
             <circle cx="12" cy="12" r="10" />
             <path d="M8 12h8" />
             <path d="M12 8v8" />
           </svg>
-          Create list
+          Create a list
         </button>
-        <button className="px-4 py-2 rounded-3xl border hover:border-orange-600 hover:bg-slate-900 transition-all ease-in-out">
-          <svg
-            className="inline-block mr-3"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          >
-            <circle cx="12" cy="12" r="10" />
-            <path d="M8 12h8" />
-            <path d="M12 8v8" />
-          </svg>
-          Learn more
+        <button
+          onClick={currentStep === 0 ? handleLearnMore : handleNext}
+          className="px-4 py-2 rounded-3xl border hover:border-orange-600 hover:bg-slate-900 transition-all ease-in-out"
+        >
+          {currentStep === 0 ? "Learn More" : "Next"}
         </button>
       </div>
+
       {/* <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
           <h1 className="font-gelica text-5xl text-white text-center mx-auto">
