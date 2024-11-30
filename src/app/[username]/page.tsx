357973a445ea3bfdba22190a6445f49a2d9fb9e0
M6K4N9R <zubchevskiy@gmail.com>
2024-11-30 19:33:31 +0100

    Change CreateListForm state in UserProfile for consistency like on home page

--- src/app/[username]/page.tsx
+++ src/app/[username]/page.tsx
@@ -3,15 +3,8 @@ import Link from "next/link";
 import CreateListForm from "../_components/forms/CreateListForm";
 
 const UserProfile = ({ user }) => {
-  const [isCreateListOpen, setCreateListOpen] = useState(false);
+    const [showCreateListForm, setShowCreateListForm] = useState<boolean>(false);
 
-  const handleCreateList = () => {
-    setCreateListOpen(true);
-  };
-
-  const handleCloseCreateList = () => {
-    setCreateListOpen(false);
-  };
 
   return (
     <section className="p-6 max-w-2xl mx-auto">
@@ -34,7 +27,7 @@ const UserProfile = ({ user }) => {
             </button>
           </Link>
           <button
-            onClick={handleCreateList}
+            onClick={() => setShowCreateListForm(true)}
             className="bg-primary-700 text-white px-4 py-2 rounded-lg"
           >
             Create List
@@ -42,7 +35,7 @@ const UserProfile = ({ user }) => {
         </div>
       </div>
 
-      {isCreateListOpen && <CreateListForm onClose={handleCloseCreateList} />}
+      {showCreateListForm && <CreateListForm onClose={() => setShowCreateListForm(false)} />}
     </section>
   );
 };
