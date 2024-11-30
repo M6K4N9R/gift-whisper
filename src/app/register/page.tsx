75587fc7d234914a057480d24e57bb1c32a9c99a
M6K4N9R <zubchevskiy@gmail.com>
2024-11-30 17:25:24 +0100

    Adjust style for register page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -36,52 +36,55 @@ export default function Register() {
   };
 
   return (
-    <section className="w-full h-screen flex items-center justify-center">
+    <section className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
       <form
         ref={ref}
         action={handleSubmit}
-        className="p-6 w-full max-w-[400px] flex flex-col justify-between items-center gap-2 
-            border border-solid border-black bg-white rounded"
+        className="p-6 w-full max-w-md flex flex-col justify-between items-center gap-4 
+        bg-dark-accent-800 text-foreground rounded-lg"
       >
-        {error && <div className="">{error}</div>}
-        <h1 className="mb-5 w-full text-2xl font-bold">Register</h1>
+        {error && <div className="text-red-500 w-full">{error}</div>}
+        <h1 className="mb-5 w-full text-2xl font-bold font-gelica text-center">
+          Register
+        </h1>
 
-        <label className="w-full text-sm">Full Name</label>
-        <input
-          type="text"
-          placeholder="Full Name"
-          className="w-full h-8 border border-solid border-black py-1 px-2.5 rounded text-[13px]"
-          name="name"
-        />
+        <div className="w-full">
+          <label className="block mb-2">Full Name</label>
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="w-full p-2 bg-dark-accent-700 border-dark-accent-700 text-foreground rounded-lg"
+            name="name"
+          />
+        </div>
 
-        <label className="w-full text-sm">Email</label>
-        <input
-          type="email"
-          placeholder="Email"
-          className="w-full h-8 border border-solid border-black py-1 px-2.5 rounded"
-          name="email"
-        />
+        <div className="w-full">
+          <label className="block mb-2">Email</label>
+          <input
+            type="email"
+            placeholder="Email"
+            className="w-full p-2 bg-dark-accent-700 border-dark-accent-700 text-foreground rounded-lg"
+            name="email"
+          />
+        </div>
 
-        <label className="w-full text-sm">Password</label>
-        <div className="flex w-full">
+        <div className="w-full">
+          <label className="block mb-2">Password</label>
           <input
             type="password"
             placeholder="Password"
-            className="w-full h-8 border border-solid border-black py-1 px-2.5 rounded"
+            className="w-full p-2 bg-dark-accent-700 border-dark-accent-700 text-foreground rounded-lg"
             name="password"
           />
         </div>
 
-        <button
-          className="w-full border border-solid border-black py-1.5 mt-2.5 rounded
-            transition duration-150 ease hover:bg-black"
-        >
+        <button className="w-full px-4 py-2 bg-primary-700 hover:bg-primary-600 text-foreground rounded-lg mt-4">
           Sign up
         </button>
 
         <Link
           href="/login"
-          className="text-sm text-[#888] transition duration-150 ease hover:text-black"
+          className="text-secondary-900 hover:text-secondary-700 mt-2"
         >
           Already have an account?
         </Link>
