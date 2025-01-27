9aeb802a38c3ee79dcf366d6b7de9fae9ff6753f
M6K4N9R <zubchevskiy@gmail.com>
2025-01-27 22:24:59 +0100

    Move username/page and archive components to (dashboard/[username])

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,6 +1,6 @@
-"use server"
+"use server";
 
-import { connectDB } from "@/lib/mongodb";
+import { connectDB } from "@/app/lib/mongodb";
 import { signIn } from "next-auth/react";
 
 import User from "@/models/User";
@@ -8,62 +8,50 @@ import User from "@/models/User";
 import bcrypt from "bcryptjs";
 
 export const register = async (values: any) => {
+  const { email, password, name } = values;
 
-    const { email, password, name } = values;
+  try {
+    await connectDB();
 
-    try {
+    const userFound = await User.findOne({ email });
 
-        await connectDB();
-
-        const userFound = await User.findOne({ email });
-
-        if(userFound){
-
-            return {
-
-                error: 'Email already exists!'
-
-            }
-
-        }
-
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        const user = new User({
-
-          name,
-
-          email,
-
-          password: hashedPassword,
-
-        });
-
-        const savedUser = await user.save();
-
-    }catch(e){
-
-        console.log(e);
-
+    if (userFound) {
+      return {
+        error: "Email already exists!",
+      };
     }
 
-}
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new User({
+      name,
+
+      email,
+
+      password: hashedPassword,
+    });
+
+    const savedUser = await user.save();
+  } catch (e) {
+    console.log(e);
+  }
+};
 
 export async function authenticate(
-    prevState: string | undefined,
-    formData: FormData
-  ) {
-    try {
-      await signIn("credentials", formData);
-    } catch (error) {
-      if (error instanceof AuthError) {
-        switch (error.type) {
-          case "CredentialsSignin":
-            return "Invalid Credentials.";
-          default:
-            "Something went wrong";
-        }
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn("credentials", formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return "Invalid Credentials.";
+        default:
+          "Something went wrong";
       }
-      throw error;
     }
+    throw error;
   }
+}
