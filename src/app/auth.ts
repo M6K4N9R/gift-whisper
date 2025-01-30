68d266b4708b6e6dc293cab6f81d538d9f8da51d
M6K4N9R <zubchevskiy@gmail.com>
2025-01-30 18:01:52 +0100

    Add getUser() to auth.ts

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -2,18 +2,39 @@ import NextAuth from "next-auth";
 import { authConfig } from "./auth.config";
 import Credentials from "next-auth/providers/credentials";
 import { z } from "zod";
-import type { User } from '@/app/lib/definitions';
-import bcrypt from 'bcrypt';
+import { User } from "@/models/User";
+import bcrypt from "bcrypt";
+import { connectDB } from "./actions/mongodb";
 
-export const { auth, signIn, signOut } = NextAuth({
-  ...authConfig,
-  providers: [
-    Credentials({
-      async authorize(credentials) {
-        const parsedCredentials = z
-          .object({ email: z.string().email(), password: z.string().min(6) })
-          .safeParse(credentials);
-      },
-    }),
-  ],
-});
+async function getUser(email: string): Promise<User | undefined> {
+    try {
+        await connectDB();
+      const user = await User.findOne({email: email}).select("+password");
+      console.log("getUser(). User is: ", user)
+      return user;
+    } catch (error) {
+      console.error('Failed to fetch user:', error);
+      throw new Error('Failed to fetch user.');
+    }
+  }
+
+  export const { auth, signIn, signOut } = NextAuth({
+    ...authConfig,
+    providers: [
+      Credentials({
+        async authorize(credentials) {
+          const parsedCredentials = z
+            .object({ email: z.string().email(), password: z.string().min(6) })
+            .safeParse(credentials);
+   
+          if (parsedCredentials.success) {
+            const { email, password } = parsedCredentials.data;
+            const user = await getUser(email);
+            if (!user) return null;
+          }
+   
+          return null;
+        },
+      }),
+    ],
+  });
