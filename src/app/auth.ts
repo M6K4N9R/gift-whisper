696788626642cd780c7416a3ed0644354e87958c
M6K4N9R <zubchevskiy@gmail.com>
2025-01-30 18:06:37 +0100

    Move /actions to lib/actions

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -4,37 +4,39 @@ import Credentials from "next-auth/providers/credentials";
 import { z } from "zod";
 import { User } from "@/models/User";
 import bcrypt from "bcrypt";
-import { connectDB } from "./actions/mongodb";
+import { connectDB } from "./lib/actions/mongodb";
 
 async function getUser(email: string): Promise<User | undefined> {
-    try {
-        await connectDB();
-      const user = await User.findOne({email: email}).select("+password");
-      console.log("getUser(). User is: ", user)
-      return user;
-    } catch (error) {
-      console.error('Failed to fetch user:', error);
-      throw new Error('Failed to fetch user.');
-    }
+  try {
+    await connectDB();
+    const user = await User.findOne({ email: email }).select("+password");
+    console.log("getUser(). User is: ", user);
+    return user;
+  } catch (error) {
+    console.error("Failed to fetch user:", error);
+    throw new Error("Failed to fetch user.");
   }
+}
 
-  export const { auth, signIn, signOut } = NextAuth({
-    ...authConfig,
-    providers: [
-      Credentials({
-        async authorize(credentials) {
-          const parsedCredentials = z
-            .object({ email: z.string().email(), password: z.string().min(6) })
-            .safeParse(credentials);
-   
-          if (parsedCredentials.success) {
-            const { email, password } = parsedCredentials.data;
-            const user = await getUser(email);
-            if (!user) return null;
-          }
-   
-          return null;
-        },
-      }),
-    ],
-  });
+export const { auth, signIn, signOut } = NextAuth({
+  ...authConfig,
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(6) })
+          .safeParse(credentials);
+
+        if (parsedCredentials.success) {
+          const { email, password } = parsedCredentials.data;
+          const user = await getUser(email);
+          if (!user) return null;
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+          if (passwordsMatch) return user;
+        }
+        console.log("Invalid credentials");
+        return null;
+      },
+    }),
+  ],
+});
