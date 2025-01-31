bbcfe7ef29436f19d19cc9e724a6eca235846161
M6K4N9R <zubchevskiy@gmail.com>
2025-01-31 16:43:11 +0100

    Apply new login form.

--- src/app/_components/forms/loginForm.tsx
+++ src/app/_components/forms/loginForm.tsx
@@ -1,62 +1,82 @@
 "use client";
 
-import { signIn } from "@/app/auth";
-// import { useActionState } from "react";
-// import { authenticate } from "@/app/lib/actions";
-// import { useSearchParams } from "next/navigation";
-import { useState } from "react";
+"use client";
+
+import Button from "@/app/_components/Button";
+import { useActionState } from "react";
+import { authenticate } from "@/app/lib/actions";
+import { useSearchParams } from "next/navigation";
 
 export default function LoginForm() {
-  const [error, setError] = useState();
-  // const searchParams = useSearchParams();
-  // const callbackUrl = searchParams.get("callbackUrl") || "/dashboard";
-  // const [errorMessage, formAction, isPending] = useActionState(
-  //   authenticate,
-  //   undefined
-  // );
-
-  async function handleLogin(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-    const formData = new FormData(event.currentTarget);
-    const email = formData.get("email");
-    const password = formData.get("password");
-
-    const result = await signIn("credentials", {
-      email: email as string,
-      password: password as string,
-      redirect: false,
-    });
-
-    if (result?.error) {
-      setError(result.error);
-    } else {
-      console.log("Login successful. Redirecting...");
-      alert("Login successful! You will be redirected to your dashboard.");
-    }
-  }
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get("callbackUrl") || "/dashboard";
+  const [errorMessage, formAction, isPending] = useActionState(
+    authenticate,
+    undefined
+  );
 
   return (
-    <form onSubmit={handleLogin}>
-      {error && <div className="error">{error}</div>}
-      <input
-        type="email"
-        name="email"
-        placeholder="Email"
-        required
-        className="text-dark-accent-900"
-      />
-      <input
-        type="password"
-        name="password"
-        placeholder="Password"
-        required
-        className="text-dark-accent-900"
-      />
-      <button type="submit">Login</button>
+    <form action={formAction} className="space-y-3">
+      <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
+        <h1 className={`mb-3 text-2xl`}>Please log in to continue.</h1>
+        <div className="w-full">
+          <div>
+            <label
+              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <div className="relative">
+              <input
+                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                id="email"
+                type="email"
+                name="email"
+                placeholder="Enter your email address"
+                required
+              />
+            </div>
+          </div>
+          <div className="mt-4">
+            <label
+              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <div className="relative">
+              <input
+                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                id="password"
+                type="password"
+                name="password"
+                placeholder="Enter password"
+                required
+                minLength={6}
+              />
+            </div>
+          </div>
+        </div>
+        <input type="hidden" name="redirectTo" value={callbackUrl} />
+        <Button type="submit" className="mt-4 w-full" aria-disabled={isPending}>
+          Log in
+        </Button>
+        <div
+          className="flex h-8 items-end space-x-1"
+          aria-live="polite"
+          aria-atomic="true"
+        >
+          {errorMessage && (
+            <>
+              <p className="text-sm text-red-500">{errorMessage}</p>
+            </>
+          )}
+        </div>
+      </div>
     </form>
   );
 }
-
 //================================================================ Original LoginForm
 // import { signIn, getSession } from "next-auth/react";
 // import { FormEvent, useState } from "react";
